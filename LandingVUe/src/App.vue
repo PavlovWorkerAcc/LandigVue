0f284659cb909bc71e3b259f3bab9c9c85102135
Dima2323-tech <65080866+Dima2323-tech@users.noreply.github.com>
2023-01-20 14:03:03 +0200

    Ver1

---
+++ LandingVUe/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <header>
+    <div class="logo_header">
+      <img src="/src/img/logo.png" alt="">
+    </div>
+
+    <nav class="nav_header">
+      <ul class="ul_header_elem">
+        <li class="li_header_elem"><a href="">Home</a></li>
+        <li class="li_header_elem"><a href="">Community</a></li>
+        <li class="li_header_elem"><a href="">Blog</a></li>
+        <li class="li_header_elem"><a href="">Events</a></li>
+      </ul>
+    </nav>
+
+    <form class="form_header">
+      <div class="glass_logo_form_header"><img src="/src/img/MagnifyingGlass.png" style="width: 15px"></div>
+      <input class="form_header_input" type="search" placeholder="Search Here...">
+      <button class="btn_header">Log in</button>
+    </form>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+
+<script>
+
+</script>
