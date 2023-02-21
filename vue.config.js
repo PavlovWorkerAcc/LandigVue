37b7ab5bf4664d2a75ddf63040da1cc32bc4f7b4
Dima2323-tech <65080866+Dima2323-tech@users.noreply.github.com>
2023-02-21 18:19:51 +0200

    Ver5

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 let history = require('connect-history-api-fallback');
 module.exports = defineConfig({
+  publicPath: "/LandigVue/",
   transpileDependencies: true,
 })
